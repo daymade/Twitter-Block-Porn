f927d5059104c23e645241074ea5b78faa0001f2
Eol <eol@eolstudy.com>
2020-07-06 20:56:00 +0800

    v2.0 Retweeter blocking completed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Twitter Block With Love
 // @namespace   https://www.eolstudy.com
-// @version     1.1
+// @version     2.0
 // @description Block all users who love a certain tweet
 // @author      Eol
 // @run-at      document-end
@@ -17,64 +17,88 @@
 
 (_ => {
   var lang = document.documentElement.lang
-  // alert("lang = " + document.documentElement.lang)
   const translations = {
-      // add your own language here by copy one and modifying it
-    "en": {
-      lang_name: "English",
-      likedby: "Timeline: Liked by", // aria-label
-      title: "Liked by",
-      btn: "Block All",
-      success: "All Users Blocked!"
+    // Please submit a feedback on Greasyfork.com if your language is not in the list bellow
+    'en': {
+      lang_name: 'English',
+      like_title: 'Liked by',
+      like_list_identifier: 'Timeline: Liked by', // aria-label
+      retweet_title: 'Retweets',
+      mini_retweet_title: 'Retweeted by',
+      retweet_list_identifier: 'Timeline: Retweeted by',
+      btn: 'Block All',
+      success: 'All Users Blocked!'
     },
-    "en-GB": {
-      lang_name: "British English",
-      likedby: "Timeline: Liked by",
-      title: "Liked by",
-      btn: "Block All",
-      success: "All Users Blocked!"
+    'en-GB': {
+      lang_name: 'British English',
+      like_title: 'Liked by',
+      like_list_identifier: 'Timeline: Liked by',
+      retweet_title: 'Retweets',
+      mini_retweet_title: 'Retweeted by',
+      retweet_list_identifier: 'Timeline: Retweeted by',
+      btn: 'Block All',
+      success: 'All Users Blocked!'
     },
-    "zh": {
-      lang_name: "简体中文",
-      likedby: '时间线：喜欢者',
-      title: "喜欢者",
-      btn: "拉清单",
-      success: "已屏蔽所有为这条推文打心的账户！"
+    'zh': {
+      lang_name: '简体中文',
+      like_title: '喜欢者',
+      like_list_identifier: '时间线：喜欢者',
+      retweet_title: '转推',
+      retweet_list_identifier: '时间线：转推者',
+      btn: '全部屏蔽',
+      success: '列表用户已全部屏蔽！'
     },
-    "zh-Hant": {
-      lang_name: "正體中文",
-      likedby: '時間軸：已被喜歡',
-      title: "已被喜歡",
-      btn: "全部封鎖",
-      success: "已封鎖所有喜歡了這條推文的賬戶！"
+    'zh-Hant': {
+      lang_name: '正體中文',
+      like_title: '已被喜歡',
+      like_list_identifier: '時間軸：已被喜歡',
+      retweet_title: '轉推',
+      retweet_list_identifier: '時間軸：已被轉推',
+      btn: '全部封鎖',
+      success: '列表用戶已全部封鎖！'
     },
-    "ja": {
-      lang_name: "日本語",
-      likedby: "タイムライン: いいねしたユーザー",
-      title: "いいねしたユーザー",
-      btn: "一括でブロック",
-      success: "全てブロックした！"
+    'ja': {
+      lang_name: '日本語',
+      like_list_identifier: 'タイムライン: いいねしたユーザー',
+      like_title: 'いいねしたユーザー',
+      retweet_list_identifier: 'タイムライン: リツイートしたユーザー',
+      retweet_title: 'リツイート',
+      btn: '<span style="font-size: small">全部ブロックする<span>',
+      success: '全てブロックしました！'
       }
   }
-  var i18n = (translations[lang])
-  if (!i18n) {
+  var i18n = translations[lang]
+
+  // lang is empty in some error pages, so check lang first
+  if (lang && !i18n) {
     var langnames = []
-    for(let l in translations) langnames.push(translations[l].lang_name)
-    langnames = langnames.join(", ")
-    alert("Twitter Block With Love userscript does not support your language. Please add your language to the translations string in the editor, or switch the language of Twitter Web App to the following languages: " + langnames + ".")
-    }
+    Object.values(translations).forEach(language => langnames.push(language.lang_name))
+    langnames = langnames.join(', ')
+    alert(
+      'Twitter Block With Love userscript does not support your language.\n' +
+      'Please submit a feedback at Greasyfork.com or a issue at Github.com.\n' +
+      'Before that, you can edit the userscript yourself or just switch the language of the Twitter Web App to any of the following languages: ' +
+      langnames + '.'
+    )
+  }
 
   function get_cookie (cname) {
-    var name = cname + "="
+    var name = cname + '='
     var ca = document.cookie.split(';')
-    for(var i=0; i<ca.length; i++)
-    {
+    for(var i=0; i<ca.length; i++) {
       var c = ca[i].trim()
       if (c.indexOf(name)==0) {
         return c.substring(name.length,c.length)
       }
     }
-    return ""
+    return ''
+  }
+
+  function get_ancestor (dom, level) {
+    for (let i = 0; i < level; ++i) {
+      dom = dom.parent()
+    }
+    return dom
   }
 
   const ajax = axios.create({
@@ -93,16 +117,25 @@
     return location.href.split('status/')[1].split('/')[0]
   }
 
+  // fetch_likers and fetch_no_comment_retweeters need to be merged into one function
   async function fetch_likers (tweetId) {
     const users = await ajax.get(`/2/timeline/liked_by.json?tweet_id=${tweetId}`).then(
       res => res.data.globalObjects.users
     )
 
     let likers = []
-    for (let user in users) {
-      likers.push(users[user].id_str)
-    }
+    Object.keys(users).forEach(user => likers.push(user)) // keys of users are id strings
     return likers
+  }
+
+  async function fetch_no_comment_retweeters (tweetId) {
+    const users = await ajax.get(`/2/timeline/retweeted_by.json?tweet_id=${tweetId}`).then(
+      res => res.data.globalObjects.users
+    )
+
+    let targets = []
+    Object.keys(users).forEach(user => targets.push(user))
+    return targets
   }
 
   function block_user (id) {
@@ -110,35 +143,52 @@
       user_id: id
     }), {
       headers: {
-        "Content-Type":'application/x-www-form-urlencoded'
+        'Content-Type':'application/x-www-form-urlencoded'
       }
     })
   }
 
+  // block_all_liker and block_no_comment_retweeters need to be merged
   async function block_all_likers () {
-    const tweet_id = get_tweet_id()
-    const likers = await fetch_likers(tweet_id)
+    const tweetId = get_tweet_id()
+    const likers = await fetch_likers(tweetId)
     likers.forEach(id => block_user(id))
   }
 
-  function clear_view () {
-    const container = $('div[aria-label="'+ i18n.likedby + '"]')
-    container.children().fadeOut(400, _ => {
-      const notice = $(`
-        <div style="
-          color: rgb(224, 36, 94);
-          text-align: center;
-          margin-top: 3em;
-          font-size: x-large;
-        ">
-          <span>${i18n.success}</span>
-        </div>
-      `)
-      container.append(notice)
-    })
+  async function block_no_comment_retweeters () {
+    const tweetId = get_tweet_id()
+    const retweeters = await fetch_no_comment_retweeters(tweetId)
+    retweeters.forEach(id => block_user(id))
+
+    const tabName = location.href.split('retweets/')[1]
+    if (tabName === 'with_comments') {
+      if (!block_no_comment_retweeters.hasAlerted) {
+        block_no_comment_retweeters.hasAlerted = true
+        alert('TBWL has only blocked users that retweeted without comments.\n Please block users with comments manually.')
+      }
+    }
   }
 
-  function mount_block_button (dom) {
+  function success_notice (identifier) {
+    return _ => {
+      const container = $('div[aria-label="'+ identifier + '"]')
+      container.children().fadeOut(400, _ => {
+        const notice = $(`
+          <div style="
+            color: rgb(224, 36, 94);
+            text-align: center;
+            margin-top: 3em;
+            font-size: x-large;
+          ">
+            <span>${i18n.success}</span>
+          </div>
+        `)
+        container.append(notice)
+      })
+    }
+  }
+
+  function mount_block_button (dom, executer, success_notifier) {
     const btn_mousedown = 'bwl-btn-mousedown'
     const btn_hover = 'bwl-btn-hover'
 
@@ -208,16 +258,27 @@
         $(this).addClass(btn_hover)
       }
     })
-    .click(block_all_likers)
-    .click(clear_view)
+    .click(executer)
+    .click(success_notifier)
 
     dom.append(button)
   }
 
   function main () {
-    waitForKeyElements('h2:has(> span:contains(' + i18n.title + '))', dom => {
-      mount_block_button(dom.parent().parent().parent())
+    waitForKeyElements('h2:has(> span:contains(' + i18n.like_title + '))', dom => {
+      mount_block_button(get_ancestor(dom, 3), block_all_likers, success_notice(i18n.like_list_identifier))
     })
+
+    waitForKeyElements('h2:has(> span:contains(' + i18n.retweet_title + '))', dom => {
+      mount_block_button(get_ancestor(dom, 3), block_no_comment_retweeters, success_notice(i18n.retweet_list_identifier))
+    })
+
+    // some languages do not need the 'mini' version
+    if (i18n.mini_retweet_title) {
+      waitForKeyElements('h2:has(> span:contains(' + i18n.mini_retweet_title + '))', dom => {
+        mount_block_button(get_ancestor(dom, 3), block_no_comment_retweeters, success_notice(i18n.retweet_list_identifier))
+      })
+    }
   }
 
   main()
